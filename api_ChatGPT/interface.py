05776b9d6ef4f80849ba7dd0f1164558ddfffce4
Hasan Ali Caliskan <hasanali7295@gmail.com>
2024-06-04 17:37:06 +0300

    Update interface.py

--- api_ChatGPT/interface.py
+++ api_ChatGPT/interface.py
@@ -1,23 +1,16 @@
-from flask import Flask, render_template
-from openai import OpenAI
-import os
-from dotenv import load_dotenv, find_dotenv
-from api_ChatGPT import prompts
+import prompts
+import google.generativeai as genai
 
-_ = load_dotenv(find_dotenv())
-client = OpenAI(
-    api_key="sk-proj-dqioiPTKvbYHpzWgTBt4T3BlbkFJEXgajeHF9RLQ2CwBCRDo"
-)
+genai.configure(api_key="AIzaSyAhGHuv2iHGDnNiXmnZpt5Al6Ao9X07_PY")
 
-models_newer = ["gpt-3.5-turbo", "gpt-4-turbo-preview", "gpt-4"]        #endpoint -> https://api.openai.com/v1/chat/completions
-models_old = ["gpt-3.5-turbo-instruct", "babbage-002", "davinci-002"]   #endpoint -> https://api.openai.com/v1/completions
+generation_config = {
+    "temperature": 0.9,
+    "top_p": 1,
+    "top_k": 1,
+    "max_output_tokens": 2048,
+}
 
-model = models_newer[0]
-temperature = 1
-max_tokens = 512
-topic = ""
-
-system_message = prompts.system_messages[2]
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 def get_post(event_name, event_topic, event_purpose, target_audience, event_platform,
              participants, hosts, sponsors, social_media_tags, date_time, event_details, event_link):
@@ -37,53 +30,9 @@ def get_post(event_name, event_topic, event_purpose, target_audience, event_plat
         event_link=event_link
     )
 
-    messages = [
-            {"role": "system", "content": system_message}, 
-            {"role": "user", "content": prompt}
-    ]
+    response = model.generate_content(prompt)
 
-
-    completion = client.chat.completions.create(
-        model=model,
-        messages=messages,
-        temperature=temperature,
-        max_tokens=max_tokens,
-        top_p=1
-    )
-
-    return completion.choices[0].message.content
-
-
-# gpt-3.5-turbo max input token = 4097
-
-# List of Chat GPT Documentation
-    # Tüm API örnekleri -> https://platform.openai.com/docs/api-reference/introduction
-    # Text Generation Models -> https://platform.openai.com/docs/guides/text-generation
-    # Prompt Engineering -> https://platform.openai.com/docs/guides/prompt-engineering
-    # Prompt Examples -> https://platform.openai.com/examples
-
-# Chat completion object
-#{
-#  "id": "chatcmpl-123",
-#  "object": "chat.completion",
-#  "created": 1677652288,
-#  "model": "gpt-3.5-turbo-0125",
-#  "system_fingerprint": "fp_44709d6fcb",
-#  "choices": [{
-#    "index": 0,
-#    "message": {
-#      "role": "assistant",
-#      "content": "\n\nHello there, how may I assist you today?",
-#    },
-#    "logprobs": null,
-#    "finish_reason": "stop"
-#  }],
-#  "usage": {
-#    "prompt_tokens": 9,
-#    "completion_tokens": 12,
-#    "total_tokens": 21
-#  }
-#}
+    return response.text
      
 
 # Etkinlik Adı: Genç Girişimciler İnovasyon Zirvesi
